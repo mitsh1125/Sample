b040d1f996620b66ca84ba72a70ade19cf6c1436
KaruSenpai <gealonjhoncarlo@gmail.com>
2025-04-14 09:00:16 +0800

    Sample

--- app/src/main/java/com/example/sample/create_account.java
+++ app/src/main/java/com/example/sample/create_account.java
@@ -3,6 +3,7 @@ package com.example.sample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class create_account extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class create_account extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+
     }
 }
